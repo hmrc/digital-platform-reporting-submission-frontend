ebeff608692a6c9bc8f67db9739314f90c57be43
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-10-06 09:11:07 +0100

    Add download endpoint for FileErrors page

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,8 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "10.10.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
-    "com.beachape"      %% "enumeratum-play"               % "1.8.0"
+    "com.beachape"      %% "enumeratum-play"               % "1.8.0",
+    "org.apache.pekko"  %% "pekko-connectors-csv"          % "1.0.2"
   )
 
   val test = Seq(
