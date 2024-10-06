3f1c68f836c32b4b374c3eb0879f34f13bc08f72
Michael Wolfendale <4563722+wolfendale@users.noreply.github.com>
2024-10-06 09:51:41 +0100

    Small fixes

--- app/forms/ReportingPeriodFormProvider.scala
+++ app/forms/ReportingPeriodFormProvider.scala
@@ -31,7 +31,7 @@ class ReportingPeriodFormProvider @Inject()(clock: Clock) extends Mappings {
     val minYear = Constants.firstLegislativeYear
 
     Form(
-      "value" -> int("reportingPeriod.error.required")
+      "value" -> int("reportingPeriod.error.required", "reportingPeriod.error.wholeNumber", "reportingPeriod.error.nonNumeric")
         .verifying(minimumValue(minYear, "reportingPeriod.error.belowMinimum", args = minYear.toString))
         .verifying(maximumValue(maxYear, "reportingPeriod.error.aboveMaximum", args = maxYear.toString))
     )
