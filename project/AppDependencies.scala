85860171337e85866dada1f6c9ac4120f0f2a9bc
Divya Singh <175611912+divya-y-singh@users.noreply.github.com>
2026-01-13 11:02:40 +0000

    "DLS-12592 Dependency upgrades" (#151)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val bootstrapVersion = "10.5.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.20.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.25.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
     "com.beachape"      %% "enumeratum-play"               % "1.9.0",
