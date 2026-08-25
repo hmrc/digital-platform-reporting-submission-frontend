415c43f02fb7668deb40b7c3360e68211f75a696
gajalakshmiseetharaman <177637840+gajalakshmiseetharaman@users.noreply.github.com>
2026-08-25 10:47:53 +0100

    DLS-13334 | Added empty field as default field to have better view of selectItems in dropdown list.
    DLS-13334 | native dropdown changes - WIP
    
    DLS-13293: change auto complete style
    
    DLS-13293:change autocomplete option
    
    DLS-13293: fix for Filter on Submissions page
    
    DLS-13293:[DPRS] - Filter on Submissions page not working correctly
    
    DLS-13293:[DPRS] - Filter on Submissions page not working correctly

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,11 +3,11 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "10.7.0"
-  private val hmrcMongoVersion = "2.13.0"
+  private val hmrcMongoVersion = "2.14.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "13.9.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "13.13.0",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion,
     "com.beachape"      %% "enumeratum-play"               % "1.9.8",
